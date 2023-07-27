d05a2b395a46b19a0c72f2371ee95e2c1420faad
Ilhan Yumer <ilhan@ilhan.name>
2023-07-27 13:04:53 +0300

    Use select input

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Credit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +15,22 @@ class PaymentController extends AbstractController
      *
      * @Route("/payment", name="show_payment", priority=50)
      */
-    public function showPaymentForm(): Response
+    public function showPaymentForm(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('payment.html.twig');
+        $credits = $entityManager->getRepository(Credit::class)->findAll();
+
+        return $this->render('payment.html.twig', [
+            'credits' => $credits
+        ]);
+    }
+
+    /**
+     *
+     * @Route("/pay", methods={"POST"}, name="pay", priority=50)
+     */
+    public function pay(Request $request): Response
+    {
+        $creditId = $request->get('credit-id');
+        $amount = $request->get('amount');
     }
 }
