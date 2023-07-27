b12142570b8e4bc5188a4234bd745bfdf60b12a6
Ilhan Yumer <ilhan@ilhan.name>
2023-07-27 20:19:46 +0300

    Validate input

--- src/Controller/CreditCreator.php
+++ src/Controller/CreditCreator.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Credit;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,12 +24,22 @@ class CreditCreator extends AbstractController
     /**
      *
      * @Route("/create-credit-action", methods={"POST"}, name="show_create_credit_action", priority=50)
+     * @throws Exception
      */
     public function createCredit(Request $request, EntityManagerInterface $entityManager): Response
     {
         $fullName = $request->get('full-name');
         $amount = $request->get('amount');
         $months = $request->get('months');
+
+        if ($months < 3 or $months > 120) {
+            throw new Exception('Най малко 3 и най много 120 месеца!');
+        }
+
+        if ($amount < 0 or $amount > 80000) {
+            throw new Exception('Не може да има кредити на обща стойност повече от 80 000 лв или по малко от 0 лв!');
+        }
+
         $interestRate = 7.9;
 
         $credit = new Credit();
